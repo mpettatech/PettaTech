694966fd18cddea35c6b8b28b1926f16731b614d
mpettatech <murilo.pettatech@gmail.com>
2026-08-07 14:27:18 +0000

    ajustes

--- pettatech-site/src/components/Home.jsx
+++ pettatech-site/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar/Navbar"
-import Hero from "../components/Hero/hero.png"
+import Hero from "../components/Hero/Hero"
 import Services from "../components/Services/Services"
 import About from "../components/About/About"
 import CTA from "../components/CTA/CTA"
@@ -7,7 +7,6 @@ import Contact from "../components/Contact/Contact"
 import Footer from "../components/Footer/Footer"
 import Cases from "../components/Cases/Cases"
 import FAQ from "../components/FAQ/FAQ"
-import Pages from "../components/Pages/Pages" 
 
 export default function Home(){
 
@@ -26,6 +25,10 @@ export default function Home(){
             <CTA/>
 
             <Contact/>
+
+            <Cases/>
+
+            <FAQ/>
 
             <Footer/>
 
