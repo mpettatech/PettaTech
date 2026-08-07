584cc35c1c5e3b97bbbe53ea75ee0a6fc1bb6afc
mpettatech <murilo.pettatech@gmail.com>
2026-08-07 14:50:56 +0000

    lklklkj

--- pettatech-site/src/Home.jsx
+++ pettatech-site/src/Home.jsx
@@ -1,7 +1,6 @@
 import Navbar from "../components/Navbar/Navbar"
 import Hero from "../components/Hero/Hero"
 import Services from "../components/Services/Services"
-import About from "../components/About/About"
 import CTA from "../components/CTA/CTA"
 import Contact from "../components/Contact/Contact"
 import Footer from "../components/Footer/Footer"
