81b97e52169e3c783301a0177568cfc9fe734bf0
mpettatech <murilo.pettatech@gmail.com>
2026-08-07 14:50:56 +0000

    lklklkj

--- pettatech-site/src/Home.jsx
+++ pettatech-site/src/Home.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Navbar from "../components/Navbar/Navbar"
 import Hero from "../components/Hero/Hero"
 import Services from "../components/Services/Services"
@@ -6,6 +7,16 @@ import Contact from "../components/Contact/Contact"
 import Footer from "../components/Footer/Footer"
 import Cases from "../components/Cases/Cases"
 import FAQ from "../components/FAQ/FAQ"
+=======
+import Navbar from "./components/Navbar/Navbar"
+import Hero from "./components/Hero/Hero"
+import Services from "./components/Services/Services"
+import CTA from "./components/CTA/CTA"
+import Contact from "./components/Contact/Contact"
+import Footer from "./components/Footer/Footer"
+import Cases from "./components/Cases/Cases"
+import FAQ from "./components/FAQ/FAQ"
+>>>>>>> 41088d7 (lklklkj)
 
 export default function Home(){
 
