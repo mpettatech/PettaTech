8b1cffbf25ab52f0207461b561a1ee0d5d769e11
mpettatech <murilo.pettatech@gmail.com>
2026-08-07 16:24:30 +0000

    Serviços

--- pettatech-site/src/Home.jsx
+++ pettatech-site/src/Home.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar/Navbar"
 import Hero from "./components/Hero/Hero"
 import fotoPerfil from "./assets/fotoperfil.jpeg"
 import Services from "./components/Services/Services"
+import About from "./components/About/About"
 export default function Home(){
 
     return(
@@ -14,7 +15,7 @@ export default function Home(){
 
             <Services/> 
 
-           
+            <About/>
 
 
         </>
