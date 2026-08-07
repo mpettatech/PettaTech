546b05fad9ad36cd24beec88e3589d8efa8830bd
mpettatech <murilo.pettatech@gmail.com>
2026-08-07 14:43:01 +0000

    ajustes

---
+++ pettatech-site/src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+export default function Hero() {
+  return (
+    <section className="h-screen flex items-center justify-center">
+
+      <div>
+
+        <h1 className="text-6xl font-bold">
+          Petta Tech
+        </h1>
+
+        <p className="mt-4 text-xl text-slate-300">
+          Tecnologia que gera resultados.
+        </p>
+
+      </div>
+
+    </section>
+  );
+}
